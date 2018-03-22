2bba4ae061a1ba3f8061942544cb27cf1c68edee
Andreas Christodoulou <andreas@useaquestion.co.uk>
2018-03-22 06:46:22 +0000

    Basic authentication. Initial tidying up of auth scaffold.

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="py-1 bg-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-4">
+				<p><b>Links</b></p>
+				<p><a href="{{ route('home') }}">Homepage</a></p>
+				<p><a href="{{ route('login') }}">Login</a></p>
+				<p><a href="{{ route('register') }}">Register</a></p>
+			</div>
+			<div class="col-4">
+				<p><b>Legal</b></p>
+				<p><a href="">Cookies</a></p>
+				<p><a href="">Delivery Information</a></p>
+				<p><a href="">Privacy Policy</a></p>
+				<p><a href="">Terms and Conditions</a></p>
+			</div>
+			<div class="col-4">
+				<p><b>Social Media</b></p>
+				<p>
+					<a href="https://www.facebook.com/andachrental" style="color: #3B5998"><i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://www.instagram.com/andachrental" style="color: #8a3ab9"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://www.youtube.com/channel/UC3xEAMedMZFFvWfF54vYpTw" style="color: #ff0000"><i class="fa fa-youtube-play fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://twitter.com/andachrental" style="color: #55acee"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://www.linkedin.com/company/andachrental" style="color: #007bb5"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://www.pinterest.co.uk/andachrental" style="color: #cb2027"><i class="fa fa-pinterest fa-2x" aria-hidden="true"></i></a> 
+					<a href="https://plus.google.com/b/103169879382806665816/103169879382806665816" style="color: #dd4b39"><i class="fa fa-google-plus fa-2x" aria-hidden="true"></i></a> 
+
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<p>Copyright &copy; 2018 - @php echo date('Y') @endphp Andach Admin</p>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ resources/views/template.blade.php
@@ -0,0 +1,131 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108867511-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-108867511-1');
+    </script>
+
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="@yield('meta-description', 'Andach Admin')">
+    <meta name="author" content="">
+
+    <title>@yield('title', 'Andach Admin')</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+      <!-- Navigation -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <div id="navbar-title">
+            <a class="navbar-brand" href="/">Andach Admin</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about-us">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/open-source">Open Source</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    @if (Session::has('success'))
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="alert alert-success"><b>Success:</b> {!! Session::get('success') !!}</div>
+        </div>
+      </div>
+    </div>
+    @endif
+
+    @if (Session::has('danger'))
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="alert alert-danger"><b>Error:</b> {!! Session::get('danger') !!}</div>
+        </div>
+      </div>
+    </div>
+    @endif
+
+    @if (isset($errors))
+      @if ($errors->any())
+      <div class="container" id="errorscontainer">
+        <div class="alert alert-danger">
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+      @endif
+    @endif
+
+    <div class="container" style="background-color: #e9ecef">
+      @yield('breadcrumbs')
+    </div>
+
+    <!-- Page Content -->
+    <div class="container" id="contentcontainer">
+      @yield('content')
+    </div>
+
+    <!-- Footer -->
+    @include('footer')
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!--If we use the CDN version rather than the self-hosted it doesn't work. Yay programming!-->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.5/esm/popper.min.js"></script>-->
+    <script src="/js/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/justgage/1.2.9/justgage.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.7/raphael.min.js"></script>
+
+    @yield('javascript')
+
+  </body>
+
+</html>
